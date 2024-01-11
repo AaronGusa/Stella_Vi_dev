9b8fed9bb4d625dbd38f37bab958a1044377ff9e
Goosa2025 <gus21002@byui.edu>
2024-01-11 13:51:04 -0700

    Style guide setup

--- stella/src/app/app.module.ts
+++ stella/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StyleguideComponent } from './styleguide/styleguide.component';
+import { HomeComponent } from './home/home.component';
+import { CommonelementsComponent } from './commonelements/commonelements.component';
+import { HeaderComponent } from './commonelements/header/header.component';
+import { FooterComponent } from './commonelements/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleguideComponent,
+    HomeComponent,
+    CommonelementsComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ stella/src/app/styleguide/styleguide.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-styleguide',
+  templateUrl: './styleguide.component.html',
+  styleUrls: ['./styleguide.component.css']
+})
+export class StyleguideComponent {
+
+}
