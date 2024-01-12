d3dde86acbb3e9282051d7252f18a8f7e328a0cc
Goosa2025 <gus21002@byui.edu>
2024-01-12 10:03:34 -0700

    footer header addition, style guide setup

--- stella/src/app/app-routing.module.ts
+++ stella/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StyleguideComponent } from './styleguide/styleguide.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'style-guide', component: StyleguideComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
