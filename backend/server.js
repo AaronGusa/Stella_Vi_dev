cac726f3eb245bcb539c32e9be73fb98c27cffbb
Goosa2025 <gus21002@byui.edu>
2024-01-19 12:59:37 -0700

    Assets added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const e = require('express');
 const app = e();
-const port = 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('We have our backend express running.');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
